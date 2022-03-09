5107bf328e43cb371f40841a9f5185e9512034b5
Tawanda <tawandagdmavondo@gmail.com>
2022-03-09 20:47:56 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'app.dart';
 
 Future<void> main() async {
+  String _sharedPrefKey = "onBoard";
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  bool? viewed = prefs.getBool('onBoard');
+  bool? viewed = prefs.getBool(_sharedPrefKey);
 
   if (viewed == null || viewed == false) {
-    await prefs.setBool("onBoard", true);
-    viewed = false;
+    await prefs.setBool(_sharedPrefKey, true);
   }
-  runApp(GoTourApp(
-    isOnboardViewed: viewed,
-  ));
+  runApp(
+    GoTourApp(
+      isOnboardViewed: viewed == null ? false : viewed,
+    ),
+  );
 }
